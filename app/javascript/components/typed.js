1cabbb66b6a14217da0d8bb80964459306add11f
Pontus Berg <pontusberg95@gmail.com>
2019-07-11 10:19:27 +0900

    Done

--- app/javascript/components/typed.js
+++ app/javascript/components/typed.js
@@ -1,5 +1,4 @@
-//= require rails-ujs
-//= require_tree .
+
 
 var typed = new Typed('.form-textfield', {
   strings: ["First sentence.", "Second sentence."],
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,4 @@
-
+import Typed from '../components/typed.js';
 import "bootstrap";
 import { timer } from "../components/timer.js";
 timer();
-
