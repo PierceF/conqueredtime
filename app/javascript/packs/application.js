925ff4df37da993c93e81a8df787f666acda139e
Pierce Franklin <piush_r@hotmail.com>
2019-07-12 10:45:02 +0900

    added non working popup for journal index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,8 @@
 import Typed from '../components/typed.js';
 import 'bootstrap';
 import { timer } from '../components/timer.js';
+import { popup } from '../components/popup.js';
 
 timer();
+popup();
+
