1e639ac8205129fe028a86ba6265b523a7c3d15e
Pierce Franklin <piush_r@hotmail.com>
2019-07-13 19:22:15 +0900

    added star rating for milestone show but the form does not work properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_015300) do
+ActiveRecord::Schema.define(version: 2019_07_13_080641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_07_12_015300) do
     t.datetime "start"
     t.datetime "end"
     t.string "state"
+    t.integer "stars", default: 0
     t.index ["milestone_id"], name: "index_pomodoros_on_milestone_id"
     t.index ["user_id"], name: "index_pomodoros_on_user_id"
   end
