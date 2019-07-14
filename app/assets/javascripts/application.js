4f8931eaf96e605e5df872f360e7db7c8f05b9a2
Pontus Berg <pontusberg95@gmail.com>
2019-07-14 12:56:48 +0900

    done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-
-
-// const typed = new Typed('.form-textfield', {
-//   strings: ['First sentence.', 'Second sentence.'],
-//   typeSpeed: 30,
-// });
-
-
-//
-// document.querySelector(".group-content1").scrollIntoView();
-// document.querySelector('.button-scroll').scrollIntoView();
