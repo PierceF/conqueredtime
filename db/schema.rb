33ce405fdfefcb59126b47f692a40a7516d83e2f
Steven Tegnelia <stegnelia@gmail.com>
2019-07-14 19:11:24 +0900

    intro redone again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_015300) do
+ActiveRecord::Schema.define(version: 2019_07_14_094321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_07_12_015300) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "title"
+    t.string "image"
     t.index ["user_id"], name: "index_journals_on_user_id"
   end
 
