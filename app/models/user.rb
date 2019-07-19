dfb54a303132553ad768ec9bcd8d8fac0fdb0ab7
Steven Tegnelia <stegnelia@gmail.com>
2019-07-19 15:00:19 +0900

    green journal

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   has_many :pomodoros, dependent: :destroy
 
   after_create do
-    Journal.create!(title: "Fitness", user: self, image: "https://res.cloudinary.com/dyeffcfck/image/upload/v1563513755/abc3_iuo7ge.png")
-    Journal.create!(title: "Spirituality", user: self, image: "https://res.cloudinary.com/dyeffcfck/image/upload/v1563513843/abc2_n7tezd.png")
-    Journal.create!(title: "Lifestyle", user: self, image:"https://res.cloudinary.com/dyeffcfck/image/upload/v1563513906/abc4_ylric4.png")
-    Journal.create!(title: "Career", user: self, image: "https://res.cloudinary.com/dyeffcfck/image/upload/v1563513850/abc1_zwux6y.png")
+    Journal.create!(title: "Fitness", user: self, image: "https://res.cloudinary.com/dyeffcfck/image/upload/v1563515817/undraw_working_out_6psf_o0mivg.png")
+    Journal.create!(title: "Spirituality", user: self, image: "https://res.cloudinary.com/dyeffcfck/image/upload/v1563515959/undraw_mindfulness_scgo_desjku.png")
+    Journal.create!(title: "Lifestyle", user: self, image:"https://res.cloudinary.com/dyeffcfck/image/upload/v1563515559/undraw_Fall_is_coming_yl0x_cd5yqi.png")
+    Journal.create!(title: "Career", user: self, image: "https://res.cloudinary.com/dyeffcfck/image/upload/v1563515727/undraw_all_the_data_h4ki_odncut.png")
   end
 
 
